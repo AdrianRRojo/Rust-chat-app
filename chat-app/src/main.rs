39e91bf84f528869822f903524af0e7692a47791
Adrian Rojo <108231637+AdrianRRojo@users.noreply.github.com>
2024-03-01 02:42:56 -0500

    Successfully retrieved mysql table data

--- chat-app/src/main.rs
+++ chat-app/src/main.rs
@@ -1,14 +1,33 @@
 // use std::io;``
-// use mysql::*;
+use mysql::{prelude::Queryable, *};
 // use mysql::prelude::*;
 use std::env;
 use dotenv::dotenv;
-
-fn main() {
+#[derive(Debug)]
+struct User {
+    id: i32,
+    username: String,
+    password: String,
+    created_at: String,
+}
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    
     dotenv().ok();
     let db_url = env::var("DB_URL").expect("Failed to find url");
+    let opts = Opts::from_url(&db_url).expect("Invalid DB Url");
+    let pool = Pool::new(opts).expect("Failed to create pool");
 
-    print!("URL: {db_url}")
+    let mut conn = pool.get_conn()?;
+
+    let users: Vec<User> = conn.query_map(
+        "SELECT id, username, password, created_at FROM users",
+        |(id, username, password, created_at)| User { id, username, password, created_at },
+    )?;
+
+    for user in users {
+        println!("{:?}", user);
+    }
+    Ok(())
     // // let user = String::new();
     // let user = "Adrian";
 
