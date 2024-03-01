72146ab1f1c3eab4f807142cbeec27fab1701768
Adrian Rojo <108231637+AdrianRRojo@users.noreply.github.com>
2024-02-29 23:31:37 -0500

    test

--- chat-app/src/main.rs
+++ chat-app/src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    // let user = String::new();
+    let user = "Adrian";
+
+    println!("________");
+
+    let mut msg = String::new();
+    io::stdin()
+        .read_line(&mut msg)
+        .expect("Failed to read message");
+
+    println!("{user}: {msg}");
 }
