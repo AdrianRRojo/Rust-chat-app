770f335467830199944c14eb1a76ed6b3091cb88
Adrian Rojo <108231637+AdrianRRojo@users.noreply.github.com>
2024-03-01 02:54:44 -0500

    Sucessfully inserted data!

--- chat-app/src/main.rs
+++ chat-app/src/main.rs
@@ -1,8 +1,9 @@
 // use std::io;``
-use mysql::{prelude::Queryable, *};
-// use mysql::prelude::*;
-use std::env;
 use dotenv::dotenv;
+use mysql::prelude::*;
+use mysql::{prelude::Queryable, *};
+use std::{env, io};
+
 #[derive(Debug)]
 struct User {
     id: i32,
@@ -11,7 +12,6 @@ struct User {
     created_at: String,
 }
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    
     dotenv().ok();
     let db_url = env::var("DB_URL").expect("Failed to find url");
     let opts = Opts::from_url(&db_url).expect("Invalid DB Url");
@@ -19,9 +19,40 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut conn = pool.get_conn()?;
 
+    let mut new_user = String::new();
+    println!("Username \n");
+
+    io::stdin()
+        .read_line(&mut new_user)
+        .expect("Failed to register username");
+
+    let mut new_user_password = String::new();
+    println!("Username \n");
+
+    io::stdin()
+        .read_line(&mut new_user_password)
+        .expect("Failed to register user");
+
+    let new_user = new_user.trim();
+    let new_user_password = new_user_password.trim();
+        
+    conn.exec_drop(
+        "INSERT INTO users (username, password) VALUES (:username, :password)",
+        params! {
+            "username" => new_user,
+            "password" => new_user_password,
+        },
+    )
+    .expect("Failed to insert new user");
+
     let users: Vec<User> = conn.query_map(
         "SELECT id, username, password, created_at FROM users",
-        |(id, username, password, created_at)| User { id, username, password, created_at },
+        |(id, username, password, created_at)| User {
+            id,
+            username,
+            password,
+            created_at,
+        },
     )?;
 
     for user in users {
