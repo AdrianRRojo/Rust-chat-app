8d7628560d7981b752c9e06273092f9393d13946
Adrian Rojo <108231637+AdrianRRojo@users.noreply.github.com>
2024-03-06 13:21:45 -0500

    Can find chatrooms by code

--- chat-app/src/auth/chatroom.rs
+++ chat-app/src/auth/chatroom.rs
@@ -27,39 +27,75 @@ pub fn chatrooms(user_id: i32) -> Result<(), Box<dyn std::error::Error>> {
 
 
     println!("Chat rooms \n----------- \n");
-    
-    let find_chat_rooms: Vec<Chatrooms> = conn.exec_map(
-        "SELECT ctp.chat_room_id, cr.name FROM chat_table_permissions ctp 
-        LEFT JOIN chat_rooms cr on cr.id=ctp.chat_room_id WHERE (ctp.user_id = :user_id AND valid_access = TRUE)", 
-        params! {
-            "user_id" => &user_id,
-        }, |(id, name)| Chatrooms { id, name},
-    
-    ).expect("Failed to find valid chat rooms");
-    
-    for room in find_chat_rooms {
-        println!("{:?}", room);
-    }
+    let mut find_a_new_room = String::new();
 
-    let mut group_chat_id = String::new();
+    println!("Do you want to:
+        \n 1. find a new chat room 
+        \n 2. create a room
+        \n 3. join your a current room? \n Enter 1 or 2 or 3.");
 
-    println!("Select a chat room ID");
     io::stdin()
-        .read_line(&mut group_chat_id)
-        .expect("Error: Enter a valid ID");
+        .read_line(&mut find_a_new_room)
+        .expect("Error. Input not taken");
 
-    let group_chat_id = group_chat_id.trim();
+    let find_a_new_room = find_a_new_room.trim();
 
-    let enter_room: Vec<RoomMsgs> = conn.exec_map(
-        "SELECT msg.message, u.username FROM messages msg 
-        LEFT JOIN users u on msg.user_id=u.id WHERE (msg.group_chat_id = :group_chat_id)", 
-        params! {
-            "group_chat_id" => &group_chat_id,
-        }, |(message,username) | RoomMsgs {message, username}).expect("Could not find chatroom");
+    if find_a_new_room == "1"{
+        let mut code = String::new();
+        println!("Enter Access code");
 
-    for msgs in enter_room {
-        println!("{}: {} ", msgs.username, msgs.message);
-    };
-    super::msg::msg(group_chat_id);
+        io::stdin()
+            .read_line(&mut code)
+            .expect("Error, code was not taken");
+
+        let code = code.trim();
+
+        let find_chat_room: Vec<Chatrooms> = conn.exec_map(
+            "SELECT id, name FROM chat_rooms WHERE access_code = :code", 
+            params! {
+                "code" => &code,
+            }, |(id, name) | Chatrooms {id,  name},
+        
+        ).expect("Error finding chat room");
+
+        for room in find_chat_room {
+            println!("{:?}", room);
+        };
+    }else if find_a_new_room == "3"{
+
+        let users_chat_rooms: Vec<Chatrooms> = conn.exec_map(
+            "SELECT ctp.chat_room_id, cr.name FROM chat_table_permissions ctp 
+            LEFT JOIN chat_rooms cr on cr.id=ctp.chat_room_id WHERE (ctp.user_id = :user_id AND valid_access = TRUE)", 
+            params! {
+                "user_id" => &user_id,
+            }, |(id, name)| Chatrooms { id, name},
+    
+        ).expect("Failed to find valid chat rooms");
+    
+        for room in users_chat_rooms {
+            println!("{:?}", room);
+        }
+
+        let mut group_chat_id = String::new();
+
+        println!("Select a chat room ID");
+        io::stdin()
+            .read_line(&mut group_chat_id)
+            .expect("Error: Enter a valid ID");
+
+        let group_chat_id = group_chat_id.trim();
+
+        let enter_room: Vec<RoomMsgs> = conn.exec_map(
+            "SELECT msg.message, u.username FROM messages msg 
+            LEFT JOIN users u on msg.user_id=u.id WHERE (msg.group_chat_id = :group_chat_id)", 
+            params! {
+                "group_chat_id" => &group_chat_id,
+            }, |(message,username) | RoomMsgs {message, username}).expect("Could not find chatroom");
+    
+        for msgs in enter_room {
+            println!("{}: {} ", msgs.username, msgs.message);
+        };
+        super::msg::msg(group_chat_id);
+    }
     Ok(())
 }
