63f43f8708dfab90400c67c7e4c8b39be5d0c925
Adrian Rojo <108231637+AdrianRRojo@users.noreply.github.com>
2024-03-09 21:09:20 -0500

    Starting, making a new chat filter

--- chat-app/rust-chat-app/src-tauri/src/auth/chatroom.rs
+++ chat-app/rust-chat-app/src-tauri/src/auth/chatroom.rs
@@ -1,12 +1,12 @@
 // use core::num;
 // mod msg;
-use std::io;
 use dotenv::dotenv;
+// use std::io;
 // use mysql::prelude::*;
-use mysql::{prelude::Queryable, *};
-use std::{env};
-use rand::seq::SliceRandom;
 use crate::auth::models::Chatrooms;
+use mysql::{prelude::Queryable, *};
+use rand::seq::SliceRandom;
+use std::env;
 
 // #[derive(Debug)]
 // struct Chatrooms {
@@ -14,9 +14,8 @@ use crate::auth::models::Chatrooms;
 //     name: String,
 // }
 
-
 #[derive(Debug)]
-struct RoomMsgs{
+struct RoomMsgs {
     message: String,
     username: String,
 }
@@ -24,7 +23,6 @@ struct RoomMsgs{
 struct ChatroomAccessCode {
     access_code: String,
 }
-
 
 pub fn load_chats(user_id: i32) -> Result<Option<Chatrooms>, String> {
     dotenv().ok();
@@ -34,14 +32,19 @@ pub fn load_chats(user_id: i32) -> Result<Option<Chatrooms>, String> {
 
     let mut conn = pool.get_conn().expect("Error");
 
-    let users_chat_rooms: Vec<Chatrooms> = conn.exec_map(
-        "SELECT ctp.chat_room_id, cr.name FROM chat_table_permissions ctp
-        LEFT JOIN chat_rooms cr ON cr.id = ctp.chat_room_id WHERE ctp.user_id = :user_id", 
-        params! {
-            "user_id" => user_id, // Make sure `user_id` is the correct type, e.g., i32
-        }, 
-        |(chat_room_id, name)| Chatrooms { id: chat_room_id, name },
-    ).expect("Failed to find valid chat rooms");
+    let users_chat_rooms: Vec<Chatrooms> = conn
+        .exec_map(
+            "SELECT ctp.chat_room_id, cr.name FROM chat_table_permissions ctp
+        LEFT JOIN chat_rooms cr ON cr.id = ctp.chat_room_id WHERE ctp.user_id = :user_id",
+            params! {
+                "user_id" => user_id, // Make sure `user_id` is the correct type, e.g., i32
+            },
+            |(chat_room_id, name)| Chatrooms {
+                id: chat_room_id,
+                name,
+            },
+        )
+        .expect("Failed to find valid chat rooms");
 
     // for room in users_chat_rooms {
     //     println!("{:?}", room);
@@ -58,7 +61,67 @@ pub fn load_chats(user_id: i32) -> Result<Option<Chatrooms>, String> {
     }
 }
 
+pub fn create_chat_room(user_id: i32, new_name: String) -> Result<String, String> {
+    dotenv().ok();
+    let db_url = env::var("DB_URL").expect("Failed to find DB url");
+    let opts = Opts::from_url(&db_url).expect("Invalid DB Url");
+    let pool = Pool::new(opts).expect("Failed to create pool");
 
+    let mut conn = pool.get_conn().expect("Error");
+
+    let letters = vec![
+        'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i',
+        'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r',
+        'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z',
+    ];
+    let numbs = vec!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let letter_len = 5;
+    let num_len = 3;
+
+    let full_code_length = letter_len + num_len;
+
+    let mut letter_output: Vec<_> = letters
+        .choose_multiple(&mut rand::thread_rng(), letter_len.try_into().unwrap())
+        .collect();
+
+    let mut numbs_output: Vec<_> = numbs
+        .choose_multiple(&mut rand::thread_rng(), num_len.try_into().unwrap())
+        .collect();
+
+    let mut code_vec = vec![];
+
+    //combine the responses into 1 vec
+    code_vec.append(&mut letter_output);
+    code_vec.append(&mut numbs_output);
+
+    code_vec.shuffle(&mut rand::thread_rng());
+
+    let code: String = code_vec
+        .into_iter()
+        .take(full_code_length as usize)
+        .collect();
+
+    // println!("{}", code);
+
+    // let mut new_name = String::new();
+    // println!("Chat room name: ");
+
+    // io::stdin()
+    //     .read_line(&mut new_name)
+    //     .expect("Error reading name");
+
+    conn.exec_drop(
+        "INSERT INTO chat_rooms (access_code, name) VALUES (:code, :new_name)",
+        params! {
+            "new_name" => new_name,
+            "code" => code.clone()
+        },
+    )
+    .expect("Failed to create room");
+    // println!("Success!");
+    Ok(code)
+}
 
 // pub fn chatrooms(user_id: i32) -> Result<(), Box<dyn std::error::Error>> {
 //     dotenv().ok();
@@ -68,12 +131,11 @@ pub fn load_chats(user_id: i32) -> Result<Option<Chatrooms>, String> {
 
 //     let mut conn = pool.get_conn()?;
 
-
 //     println!("Chat rooms \n----------- \n");
 //     let mut find_a_new_room = String::new();
 
 //     println!("Do you want to:
-//         \n 1. find a new chat room 
+//         \n 1. find a new chat room
 //         \n 2. create a room
 //         \n 3. join your a current room? \n Enter 1 or 2 or 3.");
 
@@ -94,11 +156,11 @@ pub fn load_chats(user_id: i32) -> Result<Option<Chatrooms>, String> {
 //         let code = code.trim();
 
 //         let find_chat_room: Vec<Chatrooms> = conn.exec_map(
-//             "SELECT id, name FROM chat_rooms WHERE access_code = :code", 
+//             "SELECT id, name FROM chat_rooms WHERE access_code = :code",
 //             params! {
 //                 "code" => &code,
 //             }, |(id, name) | Chatrooms {id,  name},
-        
+
 //         ).expect("Error finding chat room");
 
 //         for room in find_chat_room {
@@ -107,8 +169,8 @@ pub fn load_chats(user_id: i32) -> Result<Option<Chatrooms>, String> {
 //     }else if find_a_new_room == "2"{
 //         let letters = vec!['A','a','B','b','C','c','D','d','E','e','F','f','G','g','H','h','I','i','J','j','K','k','L','l','M','m','N','n','O','o','P','p','Q','q','R','r','S','s','T','t','U','u','V','v','W','w','X','x','Y','y','Z','z'];
 //         let numbs = vec!['0','1','2','3','4','5','6','7','8','9'];
-        
-//         let letter_len = 5; 
+
+//         let letter_len = 5;
 //         let num_len = 3;
 
 //         let full_password_length = letter_len + num_len;
@@ -116,12 +178,12 @@ pub fn load_chats(user_id: i32) -> Result<Option<Chatrooms>, String> {
 //         let mut letter_output: Vec<_> = letters
 //         .choose_multiple(&mut rand::thread_rng(), letter_len.try_into().unwrap())
 //         .collect();
-        
+
 //         let mut numbs_output: Vec<_> = numbs
 //         .choose_multiple(&mut rand::thread_rng(), num_len.try_into().unwrap())
 //         .collect();
 
-//         let mut pass_vec = vec![];
+//         let mut code_vec = vec![];
 
 //         //combine the responses into 1 vec
 //         pass_vec.append(&mut letter_output);
@@ -140,8 +202,6 @@ pub fn load_chats(user_id: i32) -> Result<Option<Chatrooms>, String> {
 //             .read_line(&mut new_name)
 //             .expect("Error reading name");
 
-
-
 //         conn.exec_drop(
 //             "INSERT INTO chat_rooms (access_code, name) VALUES (:password, :new_name)",
 //             params! {
@@ -153,14 +213,14 @@ pub fn load_chats(user_id: i32) -> Result<Option<Chatrooms>, String> {
 //     }else if find_a_new_room == "3"{
 
 //         let users_chat_rooms: Vec<Chatrooms> = conn.exec_map(
-//             "SELECT ctp.chat_room_id, cr.name FROM chat_table_permissions ctp 
-//             LEFT JOIN chat_rooms cr on cr.id=ctp.chat_room_id WHERE (ctp.user_id = :user_id AND valid_access = TRUE)", 
+//             "SELECT ctp.chat_room_id, cr.name FROM chat_table_permissions ctp
+//             LEFT JOIN chat_rooms cr on cr.id=ctp.chat_room_id WHERE (ctp.user_id = :user_id AND valid_access = TRUE)",
 //             params! {
 //                 "user_id" => &user_id,
 //             }, |(id, name)| Chatrooms { id, name},
-    
+
 //         ).expect("Failed to find valid chat rooms");
-    
+
 //         for room in users_chat_rooms {
 //             println!("{:?}", room);
 //         }
@@ -175,12 +235,12 @@ pub fn load_chats(user_id: i32) -> Result<Option<Chatrooms>, String> {
 //         let group_chat_id = group_chat_id.trim();
 
 //         let enter_room: Vec<RoomMsgs> = conn.exec_map(
-//             "SELECT msg.message, u.username FROM messages msg 
-//             LEFT JOIN users u on msg.user_id=u.id WHERE (msg.group_chat_id = :group_chat_id)", 
+//             "SELECT msg.message, u.username FROM messages msg
+//             LEFT JOIN users u on msg.user_id=u.id WHERE (msg.group_chat_id = :group_chat_id)",
 //             params! {
 //                 "group_chat_id" => &group_chat_id,
 //             }, |(message,username) | RoomMsgs {message, username}).expect("Could not find chatroom");
-    
+
 //         for msgs in enter_room {
 //             println!("{}: {} ", msgs.username, msgs.message);
 //         };
@@ -196,13 +256,13 @@ pub fn load_chats(user_id: i32) -> Result<Option<Chatrooms>, String> {
 
 //         if chat_room_choice == "1"{
 //             let get_access_code: Vec<ChatroomAccessCode> = conn.exec_map(
-//                 "SELECT access_code FROM chat_rooms WHERE id = :group_chat_id", 
+//                 "SELECT access_code FROM chat_rooms WHERE id = :group_chat_id",
 //                 params! {
 //                     "group_chat_id" => group_chat_id,
 //                 },
 //                 |(access_code)| ChatroomAccessCode { access_code },
 //             ).expect("Error finding group chat");
-            
+
 //             for access_code in get_access_code {
 //                 println!("Access code: {:?}", access_code.access_code);
 //             }
--- chat-app/rust-chat-app/src/home.js
+++ chat-app/rust-chat-app/src/home.js
@@ -8,10 +8,14 @@ async function load_chats() {
             if (chatsFromInvoke && chatsFromInvoke.length > 0) {
                 const chatroomsContainer = document.querySelector('.chatrooms');
                 for (let i = 0; i < chatsFromInvoke.length; i++) {
-                    var element = document.createElement('h1');
-                    element.textContent = `${chatsFromInvoke[i].name}`;
-                    // console.log('test: ', chatsFromInvoke[i].name);
-                    chatroomsContainer.appendChild(element); 
+                    var text1 = document.createElement('h1');
+                    let text = text1.textContent = `${chatsFromInvoke[i].name}`;
+                    // chatroomsContainer.appendChild(text);
+                    let newlink = document.createElement('a');
+                    newlink.innerHTML = text;
+                    newlink.setAttribute('title', text);
+                    newlink.setAttribute('href', `/chat?=${text}/${chatsFromInvoke[i].id}`);
+                    chatroomsContainer.appendChild(newlink);
                 }
             }
         })
@@ -20,6 +24,9 @@ async function load_chats() {
         });
 }
 
+async function create_chat(chat_name, user_id){
+    console.log(chat_name, user_id)
+}
 window.addEventListener("DOMContentLoaded", () => {
     const username = sessionStorage.getItem('username');
     const usernameDisplay = document.getElementById('usernameDisplay');
@@ -28,6 +35,13 @@ window.addEventListener("DOMContentLoaded", () => {
         usernameDisplay.textContent = `${username}`;
         load_chats();
     }
-    
+    document.querySelector("#create_chat_form").addEventListener("submit", (e) => {
+        e.preventDefault();
+        let user_id = sessionStorage.getItem('userId');
+        let chat_name = document.querySelector("#chat_name");
+        let msg = document.querySelector("#msg");
+        if (user_id && chat_name){
+            create_chat(chat_name.value, user_id);
+        }
+      });
 })
-  
