7b3f2be70eab0eb62a21077adbda8a8490a745ff
Adrian Rojo <108231637+AdrianRRojo@users.noreply.github.com>
2024-03-16 16:46:33 -0400

    added password requirements

--- chat-app/rust-chat-app/src/register.js
+++ chat-app/rust-chat-app/src/register.js
@@ -1,8 +1,8 @@
 const { invoke } = window.__TAURI__.tauri;
 async function register(username, password, email) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    // console.log("Yes!");
-    invoke("register", { username: username, password: password, email: email })
+  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  // console.log("Yes!");
+  invoke("register", { username: username, password: password, email: email })
     .then(users => {
       if (users) {
         //  console.log(users[0].id);
@@ -11,57 +11,60 @@ async function register(username, password, email) {
         sessionStorage.setItem('username', users[0].username);
         // console.log(users[0].id);
         sessionStorage.setItem('userId', users[0].id);
-        
+
       }
     })
     .catch(error => {
       console.error(error); // Handle error, display register failure message
       msg.textContent = "register failed: " + error;
     });
+}
+function hasANumb(i) {
+  return /\d/.test(i); //return true if the input has a number
+}
+
+function hasASpecialCharacter(i) {
+  return /[^A-Za-z0-9]/.test(i); //return true if input has a special character
+}
+
+function checkPassword(password, confirm_pass) {
+  if (password != confirm_pass) {
+    console.log("password", password);
+    console.log("confirm_password", password);
+    msg.textContent = "Passwords do not match";
+  } else if (password.length <= 8) {
+    msg.textContent = "Password does not meet requirements.";
+  } else if (!hasANumb(password)) {
+    msg.textContent = "Password does not include a number";
+  } else if (!hasASpecialCharacter(password)) {
+    msg.textContent = "Password does not include a special character";
+  }else{
+    return true;
   }
+}
+
   window.addEventListener("DOMContentLoaded", () => {
-   let username = document.querySelector("#reg_username");
-   let password = document.querySelector("#reg_password");
-   let confirm_pass = document.querySelector("#confirm_password");
-   let email = document.querySelector("#email");
-   let msg = document.querySelector("#msg");
-   document.querySelector("#register-form").addEventListener("submit", (e) => {
-        e.preventDefault();
-        if(username.value && password.value && confirm_pass.value && email.value){
-            if (password.value != confirm_pass.value){
-                console.log("password", password);
-                console.log("confirm_password", password);
-                msg.textContent = "Passwords do not match";
-            }else{
-                register(username.value, password.value, email.value);
-            }
-        }else{
-            console.log("username", username);
-            console.log("password", password);
-            console.log("confirm_password", password);
-            console.log("email", email);
-            msg.textContent = "Please fill in all fields";
-        }
+    let username = document.querySelector("#reg_username");
+    let password = document.querySelector("#reg_password");
+    let confirm_pass = document.querySelector("#confirm_password");
+    let email = document.querySelector("#email");
+    let msg = document.querySelector("#msg");
+    document.querySelector("#register-form").addEventListener("submit", (e) => {
+      e.preventDefault();
+      if (username.value && password.value && confirm_pass.value && email.value) {
+          if(checkPassword(password.value, confirm_pass.value)){
+            register(username.value, password.value, email.value);
+          }else{
+            msg.textContent = "Password must meet all requirements";
+          }
+      } else {
+        console.log("username", username);
+        console.log("password", password);
+        console.log("confirm_password", password);
+        console.log("email", email);
+        msg.textContent = "Please fill in all fields";
+      }
     });
-});
+  });
 
 
-//    if(username && password && confirm_pass && email){
-//         if (password != confirm_pass){
-//             msg.textContent = "Passwords do not match";
-//         }else{
-//             document.querySelector("#register-form").addEventListener("submit", (e) => {
-//                 e.preventDefault();
-//                 register();
-//               });
-//         }
-//     }else{
-//         console.log("username", username);
-//         console.log("password", password);
-//         console.log("confirm_password", password);
-//         console.log("email", email);
-//     }
-//   });
-  
-  
-  
