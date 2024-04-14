3734ce0e8ce8b4d7bbcce687574f33b0bf1d8b2c
Adrian Rojo <108231637+AdrianRRojo@users.noreply.github.com>
2024-04-14 09:43:51 -0400

    Moved chats to home page

--- chat-app/rust-chat-app/src/home.js
+++ chat-app/rust-chat-app/src/home.js
@@ -16,7 +16,7 @@ async function load_chats() {
                     newlink.innerHTML = text;
                     newlink.setAttribute('id', 'chatroom_links');
                     // newlink.setAttribute('href', `/charoom.html?name=${text}&id=${chatsFromInvoke[i].id}`);
-                    newlink.href = `/chatroom.html?name=${text}&id=${chatsFromInvoke[i].id}`;
+                    newlink.href = `/home.html?name=${text}&id=${chatsFromInvoke[i].id}`;
                     chatroomsContainer.appendChild(newlink);
                 }
             }
@@ -47,6 +47,91 @@ async function join_chat_room(user_id, access_code){
             console.warn(error);
         })
 }
+async function load_msgs(chat_id) {
+    invoke("load_msgs", { chatId: parseInt(chat_id, 10) })
+        .then(msgsFromChat => {
+            // console.log(msgsFromChat);
+            if (msgsFromChat && msgsFromChat.length > 0) {
+                console.log(msgsFromChat);
+                        const chatroomsContainer = document.querySelector('#chatroom-content');
+                        for (let i = 0; i < msgsFromChat.length; i++) {
+                            var pTag = document.createElement('p');
+                            var user = msgsFromChat[i].username;
+                            var message = msgsFromChat[i].message;
+                        
+                            var sTag = document.createElement('span');
+                            var userSpan = document.createElement('span'); 
+                            userSpan.innerText = ` ${user} `;
+                        
+                            if(user == sessionStorage.getItem('username')){
+                                
+                                sTag.innerText = message;
+                                userSpan.setAttribute('class', 'users-chat-name');
+                                sTag.setAttribute('class', 'user-message');
+                                pTag.appendChild(sTag); 
+                                pTag.appendChild(userSpan);
+                        
+                                pTag.setAttribute('class', 'user-chatMsgs');
+                            } else {
+                            
+                                sTag.innerText = message;
+                                sTag.setAttribute('class', 'message');
+                                userSpan.setAttribute('class', 'friends-chat-name');
+                                pTag.appendChild(userSpan); 
+                                pTag.appendChild(sTag); 
+                        
+                                pTag.setAttribute('class', 'chatMsgs');
+                            }
+                        
+                            chatroomsContainer.appendChild(pTag);
+                        }                                            
+
+            } else {
+                console.log(msgsFromChat);
+            }
+        }).catch(error => {
+            console.warn(error);
+        })
+}
+
+async function send_msg(chat_id, user_id, user_msg) { 
+    invoke("send_msg", { chatId: chat_id, userId: user_id, userMsg: user_msg })
+        .then(msg => {
+            console.log(msg);
+            location.reload();
+
+        }).catch(error => {
+            console.warn(error);
+        })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const params = new URLSearchParams(window.location.search);
+    const chatName = params.get('name');
+    const chat_id = params.get('id');
+
+    // Update the page title and chatroom name placeholder
+    document.title = chatName;
+    document.getElementById('chatroom-name').textContent = chatName;
+    // document.getElementById('chatroom-content').textContent = `Welcome to ${chatName}! Chat ID is ${chat_id}.`;
+
+    load_msgs(chat_id);
+
+    document.querySelector("#msg_form").addEventListener("submit", (e) => {
+        e.preventDefault();
+        // console.log('testing');
+        let user_id = sessionStorage.getItem('userId');
+        let user_msg = document.querySelector('#user_msg')
+        if (user_id && user_msg) {
+            send_msg(chat_id, user_id, user_msg.value);
+            // console.log(access_code.value);
+        } else {
+            console.warn("Chat: ", chat_id, "User: ", user_id, "Msg: ", user_msg.value);
+        }
+    });
+
+});
+
 window.addEventListener("DOMContentLoaded", () => {
     const username = sessionStorage.getItem('username');
     // const usernameDisplay = document.getElementById('usernameDisplay');
